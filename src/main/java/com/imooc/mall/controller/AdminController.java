63cf3d52f33ca0b658802a2e365ca73516b5039c
sutao-go <1565863804@qq.com>
2020-09-03 09:16:24 +0800

    2020年9月3日提交

--- src/main/java/com/imooc/mall/controller/AdminController.java
+++ src/main/java/com/imooc/mall/controller/AdminController.java
@@ -1,12 +1,14 @@
 package com.imooc.mall.controller;
 
 import com.google.code.kaptcha.Producer;
+import com.imooc.mall.entity.adminUser;
 import com.imooc.mall.service.adminUserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
@@ -18,20 +20,17 @@ import java.util.Map;
 @RequestMapping("/admin")
 public class AdminController {
     @Autowired
-    private adminUserService adminUserService;
+    private adminUserService AdminUserService;
     @Autowired
     private Producer kaptchaProducer;
-
     @RequestMapping(method = RequestMethod.GET,value = "/index")
     public String index(){
         return "templates/frontPage/index";
     }
-
     @RequestMapping(method = RequestMethod.GET,value = "/login")
     public String login(){
         return "templates/frontPage/login_page";
     }
-
     @RequestMapping(method = RequestMethod.POST ,value = "/login")
     @ResponseBody
     public Map<String,String> login1(
@@ -46,14 +45,21 @@ public class AdminController {
         String kaptcha = name2.toString();
         System.out.println(info);
         if (StringUtils.isEmpty(accountnumber)){
-            info.put("resultCode","200");
-            System.out.println("1"+info);
-
-        }else{
             info.put("resultCode","100");
-            System.out.println("2"+info);
+        }if (StringUtils.isEmpty(kaptcha)){
+            info.put("resultCode","100");
         }
-        System.out.println("3"+info);
+        if (StringUtils.isEmpty(password)) {
+            info.put("resultCode","100");
+        }
+        if(accountnumber.length() != 0 && password.length() != 0 && kaptcha.length() != 0){
+            adminUser user = AdminUserService.login(accountnumber,password);
+            if (user != null){
+                info.put("resultCode","200");
+            }
+        }else{
+            System.out.println("AdminController的"+"/login"+"出大问题了");
+        }
         return info;
     }
 }
