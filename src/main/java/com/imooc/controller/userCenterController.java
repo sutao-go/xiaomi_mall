44dfd58ad4d0405b2b07f01db03d7e488188f3a3
sutao-go <1565863804@qq.com>
2020-09-26 23:00:25 +0800

    2020年9月26日提交 更新内容: 1.制作完成了修改密码的前端界面

--- src/main/java/com/imooc/controller/userCenterController.java
+++ src/main/java/com/imooc/controller/userCenterController.java
@@ -7,10 +7,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/user")
 public class userCenterController {
-
+    /**
+     * 这个主要是用来实现前端页面跳转用的，跳转到个人用户中心用的
+     */
     @RequestMapping(method = RequestMethod.GET,value = "/center")
     public String userCenter(){
         return "templates/frontPage/userCenter";
+    }
+    /**
+     *这个主要是用来实现从个人中心的"修改个人信息"跳转到"修改密码这个功能上的"
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/accountManagement")
+    public String userInformation(){
+        return "templates/frontPage/accountManagement";
     }
 
 }
