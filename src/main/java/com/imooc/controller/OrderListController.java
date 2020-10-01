f8b30ee4e9cd5764f217fdf6af6db10ea4915c74
sutao-go <1565863804@qq.com>
2020-10-01 14:23:50 +0800

    2020年10月1日提交 更新内容:         1.新增了大量的前端商品的网页

--- src/main/java/com/imooc/controller/OrderListController.java
+++ src/main/java/com/imooc/controller/OrderListController.java
@@ -37,14 +37,14 @@ public class OrderListController {
     /**
      * 这个主要是用来实现小米10网页跳转的
      */
-    @RequestMapping(method = RequestMethod.GET,value = "/xiaomi10")
+    @RequestMapping(method = RequestMethod.GET,value = "/xiaomi10pro")
     public String xiaomi10(){
-        return "templates/frontPage/CollectionOfProductPages/xiaomi_10";
+        return "templates/frontPage/CollectionOfProductPages/xiaomi_10_pro";
     }
     /**
      * 用于小米10业务处理的
      */
-    @RequestMapping(method = RequestMethod.POST,value = "/xiaomi10")
+    @RequestMapping(method = RequestMethod.POST,value = "/xiaomi10pro")
     public String doPostXiaomi10(){
         return null;
     }
@@ -68,6 +68,74 @@ public class OrderListController {
     @RequestMapping(method = RequestMethod.GET,value = "/redmi9A")
     public String redmi9A(){
         return "templates/frontPage/CollectionOfProductPages/Redmi9A";
+    }
+    /**
+     * 这个实现了小米10青春版的手机页面跳转
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/xiaomi10youth")
+    public String xiaomi10youth(){
+        return "templates/frontPage/CollectionOfProductPages/Mi10YouthEdition5G";
+    }
+    /**
+     * 这个实现了小米10页面的跳转
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/xiaomi10")
+    public String xiaomi10r(){
+        return "templates/frontPage/CollectionOfProductPages/xiaomi_10";
+    }
+    /**
+     * 这个实现了redmik30页面的跳转
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/redMiK30pro")
+    public String redmik30(){
+    return "templates/frontPage/CollectionOfProductPages/RedmiK30Pro";
+    }
+    /**
+     * 这个实现了RedmiK30ProZoomVersion页面的跳转
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/RedmiK30ProZoomVersion")
+    public String RedmiK30ProZoomVersion(){
+        return "templates/frontPage/CollectionOfProductPages/RedmiK30ProZoomVersion";
+    }
+    /**
+     * 这个实现了RedmiSmartTVX65页面的跳转
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/RedmiSmartTVX65")
+    public String RedmiSmartTVX65(){
+        return "templates/frontPage/CollectionOfProductPages/RedmiSmartTVX65";
+    }
+
+    /**
+     * 这个实现了fullScreenTVE55A页面的跳转
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/fullScreenTVE55A")
+    public String fullScreenTVE55A(){
+        return "templates/frontPage/CollectionOfProductPages/FullScreenTVE55A";
+    }
+
+    /**
+     * 这个实现了MiFullScreenTVE32C页面的跳转
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/miFullScreenTVE32C")
+    public String miFullScreenTVE32C(){
+        return "templates/frontPage/CollectionOfProductPages/MiFullScreenTVE32C";
+    }
+    /**
+     *这个实现了MijiaAirConditioning页面的跳转
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/mijiaAirConditioning")
+    public String mijiaAirConditioning(){
+        return "templates/frontPage/CollectionOfProductPages/MijiaAirConditioning";
+    }
+    /**
+     * 这个实现了小米电视4A 60英寸页面的跳转
+     */
+    @RequestMapping(method = RequestMethod.GET,value = "/miTV4A60inches")
+    public String miTV4A60inches(){
+        return "templates/frontPage/CollectionOfProductPages/MiTV4A60inches";
     }
 }
 
